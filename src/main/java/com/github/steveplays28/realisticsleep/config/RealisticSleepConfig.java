4c04f7897bde1bc9963049009307afec148ade61
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-11 13:26:17 +0200

    Improve default config, improve config screen text

--- src/main/java/com/github/steveplays28/realisticsleep/config/RealisticSleepConfig.java
+++ src/main/java/com/github/steveplays28/realisticsleep/config/RealisticSleepConfig.java
@@ -11,12 +11,13 @@ import net.fabricmc.api.Environment;
 public class RealisticSleepConfig implements ConfigData {
     @ConfigEntry.Gui.Tooltip
     public String dawnMessage = "The sun rises.";
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 200)
     @ConfigEntry.Gui.Tooltip
-    public double sleepSpeedMultiplier = 25.0;
-    @ConfigEntry.BoundedDiscrete(min = 1, max = 1000)
+    public int sleepSpeedMultiplier = 25;
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 200)
     @ConfigEntry.Gui.Tooltip
-    public int blockEntityTickSpeedMultiplier = 2;
-    @ConfigEntry.BoundedDiscrete(min = 1, max = 1000)
+    public int blockEntityTickSpeedMultiplier = 25;
+    @ConfigEntry.BoundedDiscrete(min = 1, max = 200)
     @ConfigEntry.Gui.Tooltip
-    public int chunkTickSpeedMultiplier = 2;
+    public int chunkTickSpeedMultiplier = 25;
 }
