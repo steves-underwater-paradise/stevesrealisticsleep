810bebbf331fe0b277e39282c6cfd5d25c658c43
Darion Spaargaren <djspaargaren@outlook.com>
2022-09-11 15:05:08 +0200

    Fix crash when loading mod on dedicated server

--- src/main/java/com/github/steveplays28/realisticsleep/config/RealisticSleepConfig.java
+++ src/main/java/com/github/steveplays28/realisticsleep/config/RealisticSleepConfig.java
@@ -3,10 +3,7 @@ package com.github.steveplays28.realisticsleep.config;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 
-@Environment(EnvType.CLIENT)
 @Config(name = "realisticsleep")
 public class RealisticSleepConfig implements ConfigData {
     @ConfigEntry.Gui.Tooltip
