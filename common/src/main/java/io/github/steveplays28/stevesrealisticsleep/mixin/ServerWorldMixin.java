6f07debe5cf457498be69219d99f0e94091e25f3
mg95 <66237187+themg95@users.noreply.github.com>
2024-10-08 12:38:32 +0300

    Estimate tps and not hard-code it.

--- common/src/main/java/io/github/steveplays28/stevesrealisticsleep/mixin/ServerWorldMixin.java
+++ common/src/main/java/io/github/steveplays28/stevesrealisticsleep/mixin/ServerWorldMixin.java
@@ -109,9 +109,25 @@ public abstract class ServerWorldMixin extends World implements ServerWorldExten
 	private boolean stevesrealisticsleep$shouldSkipWeather = false;
 	@Unique
 	private int stevesrealisticsleep$consecutiveSleepTicks = 0;
+	@Unique
+	private int stevesrealisticsleep$ticksSinceLastChecked = 0;
+	@Unique
+	private long stevesrealisticsleep$previousTime = System.currentTimeMillis();
+	@Unique
+	private double stevesrealisticsleep$estimatedTps = 20.0;
 
 	@Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/GameRules;getInt(Lnet/minecraft/world/GameRules$Key;)I"))
 	public void stevesrealisticsleep$tick(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
+		if (stevesrealisticsleep$ticksSinceLastChecked >= 10) {
+			long currentTime = System.currentTimeMillis();
+			stevesrealisticsleep$estimatedTps = (double) stevesrealisticsleep$ticksSinceLastChecked / (currentTime - stevesrealisticsleep$previousTime) * 1000;
+			stevesrealisticsleep$ticksSinceLastChecked = 0;
+			stevesrealisticsleep$previousTime = currentTime;
+			System.out.println(stevesrealisticsleep$estimatedTps);
+		}
+		stevesrealisticsleep$ticksSinceLastChecked += 1;
+
+
 		// Calculate seconds until awake
 		int sleepingPlayerCount = sleepManager.getSleeping();
 		int playerCount = getPlayers().size();
@@ -120,8 +136,7 @@ public abstract class ServerWorldMixin extends World implements ServerWorldExten
 				stevesrealisticsleep$timeStepPerTick
 		);
 		int timeOfDay = StevesRealisticSleepApi.getTimeOfDay(this);
-		// TODO: Don't assume the TPS is 20
-		int secondsUntilAwake = Math.abs(SleepMathUtil.calculateSecondsUntilAwake(timeOfDay, stevesrealisticsleep$timeStepPerTick, 20));
+		int secondsUntilAwake = Math.abs(SleepMathUtil.calculateSecondsUntilAwake(timeOfDay, stevesrealisticsleep$timeStepPerTick, stevesrealisticsleep$estimatedTps));
 
 		// Check if the night has (almost) ended and the weather should be skipped
 		if (secondsUntilAwake <= 2 && stevesrealisticsleep$shouldSkipWeather) {
