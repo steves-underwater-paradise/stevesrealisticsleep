8f22e1245f84f93da99a5b230f0e92ba589f5177
Steveplays <djspaargaren@outlook.com>
2026-07-23 01:55:13 +0200

    feat: :sparkles: Add a NeoForge config button

--- src/neoforge/main/java/io/github/steveplays28/stevesrealisticsleep/neoforge/StevesRealisticSleepNeoForge.java
+++ src/neoforge/main/java/io/github/steveplays28/stevesrealisticsleep/neoforge/StevesRealisticSleepNeoForge.java
@@ -1,11 +1,18 @@
 package io.github.steveplays28.stevesrealisticsleep.neoforge;
 
 import io.github.steveplays28.stevesrealisticsleep.StevesRealisticSleep;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLEnvironment;
 
 @Mod(StevesRealisticSleep.MOD_ID)
 public class StevesRealisticSleepNeoForge {
-	public StevesRealisticSleepNeoForge() {
+	public StevesRealisticSleepNeoForge(ModContainer modContainer) {
 		StevesRealisticSleep.initialize();
+
+		if (FMLEnvironment.dist == Dist.CLIENT) {
+			io.github.steveplays28.stevesrealisticsleep.neoforge.client.StevesRealisticSleepNeoForgeClient.initialize(modContainer);
+		}
 	}
 }
--- src/neoforge/main/java/io/github/steveplays28/stevesrealisticsleep/neoforge/client/StevesRealisticSleepNeoForgeClient.java
+++ src/neoforge/main/java/io/github/steveplays28/stevesrealisticsleep/neoforge/client/StevesRealisticSleepNeoForgeClient.java
@@ -1,15 +1,23 @@
 package io.github.steveplays28.stevesrealisticsleep.neoforge.client;
 
 import io.github.steveplays28.stevesrealisticsleep.client.StevesRealisticSleepClient;
+import io.github.steveplays28.stevesrealisticsleep.config.StevesRealisticSleepConfig;
+import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.api.distmarker.Dist;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 @EventBusSubscriber(value = Dist.CLIENT)
 public class StevesRealisticSleepNeoForgeClient {
+	public static void initialize(ModContainer modContainer) {
+		modContainer.registerExtensionPoint(IConfigScreenFactory.class, (minecraft, modsListScreen) -> AutoConfig.getConfigScreen(StevesRealisticSleepConfig.class, modsListScreen).get());
+	}
+
 	@SubscribeEvent
 	public static void onClientTick(ClientTickEvent.Post event) {
 		ClientLevel clientLevel = Minecraft.getInstance().level;
