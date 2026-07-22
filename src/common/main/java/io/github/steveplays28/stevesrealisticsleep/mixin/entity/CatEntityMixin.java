457bfd8d40125cb18da1c434d0cfa34b4c393313
Steveplays <djspaargaren@outlook.com>
2026-07-22 14:31:11 +0200

    fix: :bug: Remap `CatEntityMixin` from Yarn to Mojang's mappings

--- src/common/main/java/io/github/steveplays28/stevesrealisticsleep/mixin/entity/CatEntityMixin.java
+++ src/common/main/java/io/github/steveplays28/stevesrealisticsleep/mixin/entity/CatEntityMixin.java
@@ -5,13 +5,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(targets = "net/minecraft/world/entity/animal/Cat$SleepWithOwnerGoal")
+@Mixin(targets = "net/minecraft/world/entity/animal/Cat$CatRelaxOnOwnerGoal")
 public class CatEntityMixin {
 	@Unique
-	private static final float stevesrealisticsleep$ALLOWED_SKY_ANGLE_FOR_GIFT_DROPS = 0.78f;
-
-	@ModifyExpressionValue(method = "stop", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getSkyAngle(F)F"))
-	private float stevesrealisticsleep$preventUseOfSkyAngleForGiftDrops(float original) {
-		return stevesrealisticsleep$ALLOWED_SKY_ANGLE_FOR_GIFT_DROPS;
+	private static final float stevesrealisticsleep$ALLOWED_TIME_OF_DAY_FOR_GIFT_DROPS = 0.78f;
+	/**
+	* Modifies {@link net.minecraft.world.entity.animal.Cat$CatRelaxOnOwnerGoal#stop stop} to never check the sky angle before dropping gifts.
+	*/
+	@ModifyExpressionValue(method = "stop", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getTimeOfDay(F)F"))
+	private float stevesrealisticsleep$preventUseOfTimeOfDayForGiftDrops(float original) {
+		return stevesrealisticsleep$ALLOWED_TIME_OF_DAY_FOR_GIFT_DROPS;
 	}
 }
