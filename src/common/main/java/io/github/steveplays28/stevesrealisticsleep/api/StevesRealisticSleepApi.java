7dfe442489a4b8044db2b69699469345d7e24a30
Steveplays <djspaargaren@outlook.com>
2026-07-22 04:38:25 +0200

    refactor: :recycle: Rename parameters from `world` to `level`

--- src/common/main/java/io/github/steveplays28/stevesrealisticsleep/api/StevesRealisticSleepApi.java
+++ src/common/main/java/io/github/steveplays28/stevesrealisticsleep/api/StevesRealisticSleepApi.java
@@ -17,73 +17,73 @@ import static io.github.steveplays28.stevesrealisticsleep.util.SleepMathUtil.cal
  */
 public class StevesRealisticSleepApi {
 	/**
-	 * @param world The world (aka dimension)
-	 * @return The sleep progress of the world as a percentage.
+	 * @param level The level (aka dimension)
+	 * @return The sleep progress of the level as a percentage.
 	 * @since v1.8.4
 	 */
 	@SuppressWarnings("unused")
-	public static float getSleepProgress(@NotNull Level world) {
-		return ((float) (DAY_LENGTH - calculateTicksUntilAwake(getTimeOfDay(world))) / DAY_LENGTH) * 100f;
+	public static float getSleepProgress(@NotNull Level level) {
+		return ((float) (DAY_LENGTH - calculateTicksUntilAwake(getTimeOfDay(level))) / DAY_LENGTH) * 100f;
 	}
 
 	/**
-	 * @param world The world (aka dimension)
-	 * @return The time of day of the world in ticks, with the day count filtered out using a modulo operator. This means that when Minecraft returns the time of day of a world, the day count doesn't have any effect on the resulting time. Equal to <code>(int) world.getLevelData().getDayTime() % DAY_LENGTH</code>.
+	 * @param level The level (aka dimension)
+	 * @return The time of day of the level in ticks, with the day count filtered out using a modulo operator. This means that when Minecraft returns the time of day of a level, the day count doesn't have any effect on the resulting time. Equal to <code>(int) level.getLevelData().getDayTime() % DAY_LENGTH</code>.
 	 * @since v1.8.4
 	 */
-	public static int getTimeOfDay(@NotNull Level world) {
-		return (int) world.getLevelData().getDayTime() % DAY_LENGTH;
+	public static int getTimeOfDay(@NotNull Level level) {
+		return (int) level.getLevelData().getDayTime() % DAY_LENGTH;
 	}
 
 	/**
-	 * @param world The world (aka dimension)
-	 * @return Whether players are sleeping in this world (time is passing at an accelerated rate). The world will be casted to either a <code>ServerLevel</code> or <code>ClientLevel</code> depending on the side this method is called from. Prefer the sides' respective API methods instead.
+	 * @param level The level (aka dimension)
+	 * @return Whether players are sleeping in this world (time is passing at an accelerated rate). The level will be casted to either a <code>ServerLevel</code> or <code>ClientLevel</code> depending on the side this method is called from. Prefer the sides' respective API methods instead.
 	 */
 	@SuppressWarnings("BooleanMethodIsAlwaysInverted")
-	public static boolean isSleeping(@NotNull Level world) {
-		if (world.isClientSide()) {
-			return isSleeping((ClientLevel) world);
+	public static boolean isSleeping(@NotNull Level level) {
+		if (level.isClientSide()) {
+			return isSleeping((ClientLevel) level);
 		} else {
-			return isSleeping((ServerLevel) world);
+			return isSleeping((ServerLevel) level);
 		}
 	}
 
 	/**
-	 * @param world The clientside world (aka dimension)
-	 * @return Whether players are sleeping in this world (time is passing at an accelerated rate).
+	 * @param level The clientside level (aka dimension)
+	 * @return Whether players are sleeping in this level (time is passing at an accelerated rate).
 	 */
-	public static boolean isSleeping(@NotNull ClientLevel world) {
-		var players = world.players();
+	public static boolean isSleeping(@NotNull ClientLevel level) {
+		var players = level.players();
 		var sleepingPlayers = players.stream().filter(LivingEntity::isSleeping);
 		var playerCount = players.size();
 		var sleepingPlayerCount = sleepingPlayers.count();
 		double sleepingRatio = (double) sleepingPlayerCount / playerCount;
 		double sleepingPercentage = sleepingRatio * 100;
-		int playersRequiredToSleepPercentage = world.getGameRules().getInt(GameRules.RULE_PLAYERS_SLEEPING_PERCENTAGE);
+		int playersRequiredToSleepPercentage = level.getGameRules().getInt(GameRules.RULE_PLAYERS_SLEEPING_PERCENTAGE);
 
 		return sleepingPercentage >= playersRequiredToSleepPercentage;
 	}
 
 	/**
-	 * @param world The serverside world (aka dimension)
-	 * @return Whether players are sleeping in this world (time is passing at an accelerated rate).
+	 * @param level The serverside level (aka dimension)
+	 * @return Whether players are sleeping in this level (time is passing at an accelerated rate).
 	 */
-	public static boolean isSleeping(@NotNull ServerLevel world) {
-		var playerCount = world.players().size();
-		var sleepingPlayerCount = ((ServerWorldAccessor) world).getSleepStatus().amountSleeping();
+	public static boolean isSleeping(@NotNull ServerLevel level) {
+		var playerCount = level.players().size();
+		var sleepingPlayerCount = ((ServerWorldAccessor) level).getSleepStatus().amountSleeping();
 		double sleepingRatio = (double) sleepingPlayerCount / playerCount;
 		double sleepingPercentage = sleepingRatio * 100d;
-		int playersRequiredToSleepPercentage = world.getGameRules().getInt(GameRules.RULE_PLAYERS_SLEEPING_PERCENTAGE);
+		int playersRequiredToSleepPercentage = level.getGameRules().getInt(GameRules.RULE_PLAYERS_SLEEPING_PERCENTAGE);
 
 		return sleepingPercentage >= playersRequiredToSleepPercentage;
 	}
 
 	/**
-	 * @param world The serverside world (aka dimension)
+	 * @param level The serverside level (aka dimension)
 	 * @return The consecutive amount of sleep ticks. Returns <code>0</code> when no players are sleeping.
 	 */
 	@SuppressWarnings("unused")
-	public static int getConsecutiveSleepTicks(ServerLevel world) {
-		return ((ServerWorldExtension) world).consecutiveSleepTicks;
+	public static int getConsecutiveSleepTicks(ServerLevel level) {
+		return ((ServerWorldExtension) level).consecutiveSleepTicks;
 	}
 }
